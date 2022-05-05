e944ab933fe2e9939af242320a343401791d55e1
James Woods <gurufoo@gmail.com>
2022-05-04 21:45:00 -0700

    Added the Help Togglers

--- js/FieldSettings.js
+++ js/FieldSettings.js
@@ -6,8 +6,30 @@
    * This is like a "Class". Use it like a __constructor() method.
    * Add Event Handlers.
    */
-  window.FieldSettings = function () {
-    console.log("FieldSettings");
-  }
+  window.FieldSettings = function ($wrapper) {
+    this.$wrapper = $wrapper;
+    console.log("FieldSettings", $wrapper);
+
+    //	  $('.js-help-toggler')
+
+    // Event Handler for clicking on the ball
+    this.$wrapper.on(
+      'click',
+      '.js-help-toggler',
+      this.helpToggler.bind(this)
+    );
+
+  };
+
+  $.extend(window.FieldSettings.prototype, {
+
+    helpToggler: function (e) {
+      console.log("e", $(e.target));
+      console.log(this);
+
+      $(e.target).parent().next('.body-wrapper').toggleClass(['show-help']);
+    }
+
+  });
 
 })(window, jQuery);
